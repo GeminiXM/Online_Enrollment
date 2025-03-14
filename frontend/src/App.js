4304ba3cc04a381ac531f0afc02a808d721fafdb
Mark Moore <arkhos2@hotmail.com>
2025-03-14 07:49:28 -0600

    added a new landing page that looks good

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import EnrollmentForm from "./components/EnrollmentForm.jsx";
+import LandingPage from "./components/LandingPage.jsx";
 
 // Import pages
 // Example: import Home from './pages/Home.js';
@@ -11,12 +12,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tabor Center</h1>
+        <div className="header-content">
+          <h1>
+            <Link to="/" className="logo-link">
+              Tabor Center
+            </Link>
+          </h1>
+          <nav className="main-nav">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/enrollment">Enrollment</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <main>
         <Routes>
           {/* Define your routes here */}
-          <Route path="/" element={<EnrollmentForm />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/enrollment" element={<EnrollmentForm />} />
           {/* Add more routes as needed */}
         </Routes>
